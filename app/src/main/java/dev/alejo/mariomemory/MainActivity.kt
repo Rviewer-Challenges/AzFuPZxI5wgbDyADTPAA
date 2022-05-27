2b2b3487bb5712b26ad14f85322988cd417c6da1
unknown <jimmyale3102@gmail.com>
2022-05-27 18:57:43 -0500

    Init main activity

--- app/src/main/java/dev/alejo/mariomemory/MainActivity.kt
+++ app/src/main/java/dev/alejo/mariomemory/MainActivity.kt
@@ -2,10 +2,24 @@ package dev.alejo.mariomemory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import dev.alejo.mariomemory.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initUI()
+    }
+
+    private fun initUI() {
+        binding.playButton.setOnClickListener {
+            binding.playButton.visibility = View.GONE
+            binding.difficultContent.visibility = View.VISIBLE
+        }
     }
 }
