0765717826394c77c5cc136e570b70e5a836bd40
unknown <jimmyale3102@gmail.com>
2022-05-28 21:49:55 -0500

    Add characters

--- app/src/main/java/dev/alejo/mariomemory/App.kt
+++ app/src/main/java/dev/alejo/mariomemory/App.kt
@@ -8,6 +8,9 @@ class App: Application() {
         const val EASY_DIFFICULT = 0
         const val MEDIUM_DIFFICULT = 1
         const val HARD_DIFFICULT = 2
+        const val EASY_COLUMNS = 4
+        const val MEDIUM_COLUMNS = 6
+        const val HARD_COLUMNS = 6
     }
 
 }
--- app/src/main/java/dev/alejo/mariomemory/ui/MemoryBoardActivity.kt
+++ app/src/main/java/dev/alejo/mariomemory/ui/MemoryBoardActivity.kt
@@ -6,7 +6,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import dev.alejo.mariomemory.App.Companion.EASY_COLUMNS
+import dev.alejo.mariomemory.App.Companion.EASY_DIFFICULT
+import dev.alejo.mariomemory.App.Companion.HARD_COLUMNS
+import dev.alejo.mariomemory.App.Companion.HARD_DIFFICULT
+import dev.alejo.mariomemory.App.Companion.MEDIUM_COLUMNS
+import dev.alejo.mariomemory.App.Companion.MEDIUM_DIFFICULT
 import dev.alejo.mariomemory.R
 import dev.alejo.mariomemory.adapter.BlockAdapter
 import dev.alejo.mariomemory.adapter.BlockItem
@@ -17,6 +22,7 @@ import dev.alejo.mariomemory.preferences.Prefs
 class MemoryBoardActivity : AppCompatActivity(),  OnBlockItemListener{
 
     private lateinit var binding: ActivityMemoryBoardBinding
+    private var columns = EASY_COLUMNS
     private var blocks = ArrayList<BlockItem>()
     private val adapter by lazy { BlockAdapter(blocks, this) }
     private val difficult by lazy { Prefs(this).getDifficult() }
@@ -25,12 +31,17 @@ class MemoryBoardActivity : AppCompatActivity(),  OnBlockItemListener{
         super.onCreate(savedInstanceState)
         binding = ActivityMemoryBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setBlocksData()
         initUI()
-        setData()
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    private fun setData() {
+    private fun setBlocksData() {
+        when(difficult) {
+            EASY_DIFFICULT -> { columns = EASY_COLUMNS }
+            MEDIUM_DIFFICULT -> { columns = MEDIUM_COLUMNS }
+            HARD_DIFFICULT -> { columns = HARD_COLUMNS }
+        }
         blocks = ArrayList<BlockItem>().apply {
             add(BlockItem(0, R.drawable.mario_thinking))
             add(BlockItem(1, R.drawable.mario_thinking))
@@ -41,18 +52,13 @@ class MemoryBoardActivity : AppCompatActivity(),  OnBlockItemListener{
         adapter.notifyDataSetChanged()
     }
 
-    @SuppressLint("NotifyDataSetChanged")
     private fun initUI() {
-        blocks = ArrayList<BlockItem>().apply {
-            add(BlockItem(0, R.drawable.mario_thinking))
-            add(BlockItem(1, R.drawable.mario_thinking))
-            add(BlockItem(2, R.drawable.mario_thinking))
-            add(BlockItem(3, R.drawable.mario_thinking))
-            add(BlockItem(4, R.drawable.mario_thinking))
-        }
-        adapter.notifyDataSetChanged()
-        binding.blocksRecycler
-            .layoutManager = GridLayoutManager(this, 4, LinearLayoutManager.VERTICAL, false)
+        binding.blocksRecycler.layoutManager = GridLayoutManager(
+            this,
+            columns,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
         binding.blocksRecycler.adapter = adapter
     }
 
