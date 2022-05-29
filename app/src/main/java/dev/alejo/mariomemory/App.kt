a38e13475503967187ffd9b4747e6513bca5c4ea
unknown <jimmyale3102@gmail.com>
2022-05-29 12:10:19 -0500

    Set blocks data

--- app/src/main/java/dev/alejo/mariomemory/App.kt
+++ app/src/main/java/dev/alejo/mariomemory/App.kt
@@ -6,11 +6,16 @@ class App: Application() {
 
     companion object {
         const val EASY_DIFFICULT = 0
-        const val MEDIUM_DIFFICULT = 1
-        const val HARD_DIFFICULT = 2
         const val EASY_COLUMNS = 4
+        const val EASY_TOTAL_BLOCKS = 16
+
+        const val MEDIUM_DIFFICULT = 1
         const val MEDIUM_COLUMNS = 6
+        const val MEDIUM_TOTAL_BLOCKS = 24
+
+        const val HARD_DIFFICULT = 2
         const val HARD_COLUMNS = 6
+        const val HARD_TOTAL_BLOCKS = 30
     }
 
 }
--- app/src/main/java/dev/alejo/mariomemory/ui/MemoryBoardActivity.kt
+++ app/src/main/java/dev/alejo/mariomemory/ui/MemoryBoardActivity.kt
@@ -1,29 +1,37 @@
 package dev.alejo.mariomemory.ui
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import dev.alejo.mariomemory.App.Companion.EASY_COLUMNS
 import dev.alejo.mariomemory.App.Companion.EASY_DIFFICULT
+import dev.alejo.mariomemory.App.Companion.EASY_TOTAL_BLOCKS
 import dev.alejo.mariomemory.App.Companion.HARD_COLUMNS
 import dev.alejo.mariomemory.App.Companion.HARD_DIFFICULT
+import dev.alejo.mariomemory.App.Companion.HARD_TOTAL_BLOCKS
 import dev.alejo.mariomemory.App.Companion.MEDIUM_COLUMNS
 import dev.alejo.mariomemory.App.Companion.MEDIUM_DIFFICULT
+import dev.alejo.mariomemory.App.Companion.MEDIUM_TOTAL_BLOCKS
 import dev.alejo.mariomemory.R
 import dev.alejo.mariomemory.adapter.BlockAdapter
 import dev.alejo.mariomemory.adapter.BlockItem
 import dev.alejo.mariomemory.adapter.OnBlockItemListener
 import dev.alejo.mariomemory.databinding.ActivityMemoryBoardBinding
 import dev.alejo.mariomemory.preferences.Prefs
+import java.util.*
+import kotlin.collections.ArrayList
+
 
 class MemoryBoardActivity : AppCompatActivity(),  OnBlockItemListener{
 
     private lateinit var binding: ActivityMemoryBoardBinding
     private var columns = EASY_COLUMNS
+    private var totalBlocks = EASY_TOTAL_BLOCKS
     private var blocks = ArrayList<BlockItem>()
+    private var resIdsUsed = ArrayList<Int>()
     private val adapter by lazy { BlockAdapter(blocks, this) }
     private val difficult by lazy { Prefs(this).getDifficult() }
 
@@ -32,24 +40,37 @@ class MemoryBoardActivity : AppCompatActivity(),  OnBlockItemListener{
         binding = ActivityMemoryBoardBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setBlocksData()
-        initUI()
     }
 
-    @SuppressLint("NotifyDataSetChanged")
+    @SuppressLint("NotifyDataSetChanged", "Recycle")
     private fun setBlocksData() {
         when(difficult) {
-            EASY_DIFFICULT -> { columns = EASY_COLUMNS }
-            MEDIUM_DIFFICULT -> { columns = MEDIUM_COLUMNS }
-            HARD_DIFFICULT -> { columns = HARD_COLUMNS }
+            EASY_DIFFICULT -> {
+                columns = EASY_COLUMNS
+                totalBlocks = EASY_TOTAL_BLOCKS
+            }
+            MEDIUM_DIFFICULT -> {
+                columns = MEDIUM_COLUMNS
+                totalBlocks = MEDIUM_TOTAL_BLOCKS
+            }
+            HARD_DIFFICULT -> {
+                columns = HARD_COLUMNS
+                totalBlocks = HARD_TOTAL_BLOCKS
+            }
         }
-        blocks = ArrayList<BlockItem>().apply {
-            add(BlockItem(0, R.drawable.mario_thinking))
-            add(BlockItem(1, R.drawable.mario_thinking))
-            add(BlockItem(2, R.drawable.mario_thinking))
-            add(BlockItem(3, R.drawable.mario_thinking))
-            add(BlockItem(4, R.drawable.mario_thinking))
+        val characters = resources.obtainTypedArray(R.array.characters)
+        while ( blocks.size < totalBlocks) {
+            val rndInt = Random().nextInt(characters.length())
+            val resID = characters.getResourceId(rndInt, 0)
+            resIdsUsed.add(resID)
+            if(!resIdsUsed.contains(resID)) {
+                blocks.add(BlockItem(blocks.size, resID))
+                blocks.add(BlockItem(blocks.size + 1, resID))
+            }
         }
+        blocks = ArrayList(blocks.shuffled())
         adapter.notifyDataSetChanged()
+        initUI()
     }
 
     private fun initUI() {
